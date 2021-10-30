da56cad35364bfcde6b0ca7429e028a339458453
Focusly Corp <77958705+focusly777@users.noreply.github.com>
2021-10-30 19:34:06 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -24,163 +24,76 @@ import 'src/misc/hero_animation.dart';
 import 'src/misc/physics_card_drag.dart';
 import 'src/misc/repeating_animation.dart';
 
-void main() => runApp(const AnimationSamples());
+import 'package:flutter/material.dart';
 
-class Demo {
-  final String name;
-  final String route;
-  final WidgetBuilder builder;
+void main() => runApp(const MyApp());
 
-  const Demo({
-    required this.name,
-    required this.route,
-    required this.builder,
-  });
-}
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-final basicDemos = [
-  Demo(
-      name: 'AnimatedContainer',
-      route: AnimatedContainerDemo.routeName,
-      builder: (context) => const AnimatedContainerDemo()),
-  Demo(
-      name: 'PageRouteBuilder',
-      route: PageRouteBuilderDemo.routeName,
-      builder: (context) => const PageRouteBuilderDemo()),
-  Demo(
-      name: 'Animation Controller',
-      route: AnimationControllerDemo.routeName,
-      builder: (context) => const AnimationControllerDemo()),
-  Demo(
-      name: 'Tweens',
-      route: TweenDemo.routeName,
-      builder: (context) => const TweenDemo()),
-  Demo(
-      name: 'AnimatedBuilder',
-      route: AnimatedBuilderDemo.routeName,
-      builder: (context) => const AnimatedBuilderDemo()),
-  Demo(
-      name: 'Custom Tween',
-      route: CustomTweenDemo.routeName,
-      builder: (context) => const CustomTweenDemo()),
-  Demo(
-      name: 'Tween Sequences',
-      route: TweenSequenceDemo.routeName,
-      builder: (context) => const TweenSequenceDemo()),
-  Demo(
-      name: 'Fade Transition',
-      route: FadeTransitionDemo.routeName,
-      builder: (context) => const FadeTransitionDemo()),
-];
-
-final miscDemos = [
-  Demo(
-      name: 'Expandable Card',
-      route: ExpandCardDemo.routeName,
-      builder: (context) => const ExpandCardDemo()),
-  Demo(
-      name: 'Carousel',
-      route: CarouselDemo.routeName,
-      builder: (context) => CarouselDemo()),
-  Demo(
-      name: 'Focus Image',
-      route: FocusImageDemo.routeName,
-      builder: (context) => const FocusImageDemo()),
-  Demo(
-      name: 'Card Swipe',
-      route: CardSwipeDemo.routeName,
-      builder: (context) => const CardSwipeDemo()),
-  Demo(
-      name: 'Repeating Animation',
-      route: RepeatingAnimationDemo.routeName,
-      builder: (context) => const RepeatingAnimationDemo()),
-  Demo(
-      name: 'Spring Physics',
-      route: PhysicsCardDragDemo.routeName,
-      builder: (context) => const PhysicsCardDragDemo()),
-  Demo(
-      name: 'AnimatedList',
-      route: AnimatedListDemo.routeName,
-      builder: (context) => const AnimatedListDemo()),
-  Demo(
-      name: 'AnimatedPositioned',
-      route: AnimatedPositionedDemo.routeName,
-      builder: (context) => const AnimatedPositionedDemo()),
-  Demo(
-      name: 'AnimatedSwitcher',
-      route: AnimatedSwitcherDemo.routeName,
-      builder: (context) => const AnimatedSwitcherDemo()),
-  Demo(
-      name: 'Hero Animation',
-      route: HeroAnimationDemo.routeName,
-      builder: (context) => const HeroAnimationDemo()),
-  Demo(
-      name: 'Curved Animation',
-      route: CurvedAnimationDemo.routeName,
-      builder: (context) => const CurvedAnimationDemo()),
-];
-
-final basicDemoRoutes =
-    Map.fromEntries(basicDemos.map((d) => MapEntry(d.route, d.builder)));
-
-final miscDemoRoutes =
-    Map.fromEntries(miscDemos.map((d) => MapEntry(d.route, d.builder)));
-
-final allRoutes = <String, WidgetBuilder>{
-  ...basicDemoRoutes,
-  ...miscDemoRoutes,
-};
-
-class AnimationSamples extends StatelessWidget {
-  const AnimationSamples({Key? key}) : super(key: key);
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Animation Samples',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      routes: allRoutes,
-      home: const HomePage(),
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final headerStyle = Theme.of(context).textTheme.headline6;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animation Samples'),
+      body: Center(
+        child: TextField(
+          controller: _controller,
+          onSubmitted: (String value) async {
+            await showDialog<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('대본암기도우미'),
+                  content: Text('"$value"를 암기하도록 도와주겠습니다.'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('암기하러가기'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
       ),
-      body: ListView(
-        children: [
-          ListTile(title: Text('Basics', style: headerStyle)),
-          ...basicDemos.map((d) => DemoTile(demo: d)),
-          ListTile(title: Text('Misc', style: headerStyle)),
-          ...miscDemos.map((d) => DemoTile(demo: d)),
-        ],
-      ),
-    );
-  }
-}
-
-class DemoTile extends StatelessWidget {
-  final Demo demo;
-
-  const DemoTile({required this.demo, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(demo.name),
-      onTap: () {
-        Navigator.pushNamed(context, demo.route);
-      },
     );
   }
 }
