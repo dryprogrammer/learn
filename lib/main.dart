e99a2c8f43496d64e30b54ac976bb1542e05f6bc
Focusly Corp <77958705+focusly777@users.noreply.github.com>
2021-11-13 18:46:01 +0900

    modify

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@
 import 'package:flutter/material.dart';
 import 'src/basics/06_custom_tween.dart';
 
-import 'package:flutter/material.dart';
-
 void main() => runApp(const MyApp());
 
 /// This is the main application widget.
@@ -24,67 +22,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+// /// This is the stateful widget that the main application instantiates.
+// class MyStatefulWidget extends StatefulWidget {
+//   const MyStatefulWidget({Key? key}) : super(key: key);
 
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
+//   @override
+//   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+// }
 
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  late TextEditingController _controller;
+// /// This is the private State class that goes with MyStatefulWidget.
+// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+//   late TextEditingController _controller;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = TextEditingController();
+//   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Clipboard Reader'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: TextField(
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Input Text',
-          ),
-          keyboardType: TextInputType.multiline,
-          maxLines: null,
-          controller: _controller,
-          onSubmitted: (String value) async {
-            await showDialog<void>(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('대본암기도우미'),
-                  content: Text('"$value"를 암기하도록 도와주겠습니다.'),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('암기하러가기'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Clipboard Reader'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.symmetric(horizontal: 16),
+//         child: TextField(
+//           decoration: InputDecoration(
+//             border: InputBorder.none,
+//             hintText: 'Input Text',
+//           ),
+//           keyboardType: TextInputType.multiline,
+//           maxLines: null,
+//           controller: _controller,
+//           onSubmitted: (String value) async {
+//             await showDialog<void>(
+//               context: context,
+//               builder: (BuildContext context) {
+//                 return AlertDialog(
+//                   title: const Text('대본암기도우미'),
+//                   content: Text('"$value"를 암기하도록 도와주겠습니다.'),
+//                   actions: <Widget>[
+//                     TextButton(
+//                       onPressed: () {
+//                         Navigator.pop(context);
+//                       },
+//                       child: const Text('암기하러가기'),
+//                     ),
+//                   ],
+//                 );
+//               },
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
